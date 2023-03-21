683ce290e67de87f620bde5a6519a91412a6d053
Debsvalerio <103152279+Debsvalerio@users.noreply.github.com>
2023-03-21 14:09:54 -0400

    Primeiro Teste API

---
+++ cypress/e2e/api/naoLogado.cy.js
@@ -0,0 +1,12 @@
+describe('API - Profile', () => {
+   
+    context('todos os perfis', () => {
+        
+        it('valida a API de perfis', () => {
+            
+            cy.request({
+                
+            })
+        })
+    })
+})
